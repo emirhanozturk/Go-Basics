7ee159eb6c61da9f767e122e11a4168048b9f4b7
Emirhan Öztürk <68188573+emirhanozturk@users.noreply.github.com>
2022-01-22 17:05:12 +0300

    Use of labeled statements

--- word-finder/main.go
+++ word-finder/main.go
@@ -13,13 +13,14 @@ func main() {
 	words := strings.Fields(corpus)
 	query := os.Args[1:]
 
+queries:
 	for _, q := range query {
 		q = strings.ToLower(q)
 		for i, w := range words {
 			if q == w {
 				fmt.Printf("#%-2d: %q\n", i+1, w)
 
-				break
+				continue queries
 			}
 		}
 	}
