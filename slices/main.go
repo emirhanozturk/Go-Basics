0844b520de0bb14c0f13fc35bc27c3c50f822b1a
Emirhan Öztürk <68188573+emirhanozturk@users.noreply.github.com>
2022-02-12 22:41:16 +0300

    full slices expressions

--- slices/main.go
+++ slices/main.go
@@ -1,37 +1,51 @@
 package main
 
 import (
-	"fmt"
-	"math/rand"
-	"os"
-	"sort"
-	"strconv"
-	"time"
+	_ "fmt"
+	_ "math/rand"
+	_ "os"
+	_ "sort"
+	_ "strconv"
+	_ "time"
+
+	s "github.com/inancgumus/prettyslice"
 )
 
 func main() {
-	rand.Seed(time.Now().UnixNano())
+	// 	rand.Seed(time.Now().UnixNano())
 
-	max, _ := strconv.Atoi(os.Args[1])
+	// 	max, _ := strconv.Atoi(os.Args[1])
 
-	var uniques []int
+	// 	var uniques []int
 
-loop:
-	for len(uniques) < max {
-		n := rand.Intn(max) + 1
-		fmt.Printf("%d ", n)
+	// loop:
+	// 	for len(uniques) < max {
+	// 		n := rand.Intn(max) + 1
+	// 		fmt.Printf("%d ", n)
 
-		for _, unique := range uniques {
-			if unique == n {
-				continue loop
-			}
-		}
-		uniques = append(uniques, n)
-	}
-	fmt.Println("\n\n uniques: ", uniques)
+	// 		for _, unique := range uniques {
+	// 			if unique == n {
+	// 				continue loop
+	// 			}
+	// 		}
+	// 		uniques = append(uniques, n)
+	// 	}
+	// 	fmt.Println("\n\n uniques: ", uniques)
 
-	sort.Ints(uniques)
+	// 	sort.Ints(uniques)
 
-	fmt.Println("\n\n Sorted: ", uniques)
+	// 	fmt.Println("\n\n Sorted: ", uniques)
+
+	//Full slice expressions
+
+	s.PrintBacking = true
+
+	nums := []int{1, 3, 2, 4}
+	odds := append(nums[:2:2], 5, 7) //limit the capacity
+	//Unless you want to override the old slice,use full slice expression
+	evens := append(nums[2:4], 6, 8)
+	s.Show("nums", nums)
+	s.Show("odds", odds)
+	s.Show("evens", evens)
 
 }
