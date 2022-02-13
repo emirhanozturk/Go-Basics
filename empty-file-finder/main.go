2af0eba0c557e2edb5cfa30dfdc2be29890d3c19
Emirhan Öztürk <68188573+emirhanozturk@users.noreply.github.com>
2022-02-13 15:19:13 +0300

    empty-file-finder

---
+++ empty-file-finder/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	args := os.Args[1:]
+
+	if len(args) == 0 {
+		fmt.Println("Provide a directory")
+		return
+	}
+
+	files, err := ioutil.ReadDir(args[0])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	var total int
+	for _, file := range files {
+		if file.Size() == 0 {
+			total += len(file.Name()) + 1
+		}
+	}
+	fmt.Printf("Total required space %d bytes.\n", total)
+	names := make([]byte, 0, total)
+	//for allocates backing array only once
+
+	for _, file := range files {
+		if file.Size() == 0 {
+			name := file.Name()
+			names = append(names, name...)
+			names = append(names, '\n')
+		}
+	}
+	err = ioutil.WriteFile("out.txt", names, 0644)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("%s", names)
+}
--- go.mod
+++ go.mod
@@ -5,7 +5,11 @@ go 1.17
 require github.com/inancgumus/prettyslice v0.0.0-20190305220808-d802ba58098f
 
 require (
+	github.com/alecthomas/chroma v0.10.0 // indirect
+	github.com/danwakefield/fnmatch v0.0.0-20160403171240-cbb64ac3d964 // indirect
+	github.com/dlclark/regexp2 v1.4.0 // indirect
 	github.com/fatih/color v1.13.0 // indirect
+	github.com/inancgumus/godocc v0.0.0-20190905213726-069c51ef9565 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	golang.org/x/sys v0.0.0-20220209214540-3681064d5158 // indirect
