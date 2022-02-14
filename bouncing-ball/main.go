176858a6c4fdd018e4d313aa59d3c9221abd25f5
Emirhan Öztürk <68188573+emirhanozturk@users.noreply.github.com>
2022-02-14 23:59:30 +0300

    push

--- bouncing-ball/main.go
+++ bouncing-ball/main.go
@@ -5,48 +5,52 @@ import (
 	"time"
 
 	"github.com/inancgumus/screen"
+	"github.com/mattn/go-runewidth"
 )
 
 func main() {
 	const (
-		width  = 50
-		height = 10
-
 		cellEmpty = ' '
 		cellBall  = '⚾'
+
 		maxFrames = 1200
 		speed     = time.Second / 20
+
+		ivx, ivy = 5, 2
 	)
 	var (
-		px, py int
-		vx, vy = 1, 1
-		cell   rune
+		px, py   int
+		ppx, ppy int //previous ball position
+		vx, vy   = ivx, ivy
+		cell     rune
 	)
+	width, height := screen.Size()
+	ballWidth := runewidth.RuneWidth(cellBall)
+
+	width /= ballWidth
+	height--
 
 	board := make([][]bool, width)
 
 	for column := range board {
 		board[column] = make([]bool, height)
 	}
+	bufLen := (width*2 + 1) * height
 
-	buf := make([]rune, 0, width*height)
+	buf := make([]rune, 0, bufLen)
 	screen.Clear()
 	for i := 0; i < maxFrames; i++ {
 		px += vx
 		py += vy
-		if px <= 0 || px >= width-1 {
+		if px <= 0 || px >= width-ivx {
 			vx *= -1
 		}
-		if py <= 0 || py >= height-1 {
+		if py <= 0 || py >= height-ivx {
 			vy *= -1
 		}
 		//remove the board
-		for y := range board[0] {
-			for x := range board {
-				board[x][y] = false
-			}
-		}
-		board[px][py] = true
+		board[px][py], board[ppx][ppy] = true, false
+		ppx, ppy = px, py
 
 		buf = buf[:0]
 		//draw the board
