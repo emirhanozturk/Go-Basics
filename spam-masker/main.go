4482f8d4c233332cafb7b209c14cc31ff116298e
Emirhan Öztürk <68188573+emirhanozturk@users.noreply.github.com>
2022-03-02 01:48:58 +0300

    masking spam links to learn how to manipulating the bytes and strings.

---
+++ spam-masker/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	args := os.Args[1:]
+
+	if len(args) != 1 {
+		fmt.Println("Please give argumants")
+		return
+	}
+
+	const (
+		link  = "http://"
+		nlink = len(link)
+		mask  = '*'
+	)
+
+	var (
+		text = args[0]
+		size = len(text)
+		buf  = make([]byte, 0, size)
+
+		in bool
+	)
+
+	for i := 0; i < size; i++ {
+		if len(text[i:]) >= nlink && text[i:i+nlink] == link {
+			in = true
+			buf = append(buf, link...)
+			i += nlink
+		}
+
+		c := text[i]
+
+		switch c {
+		case ' ', '\t', '\n':
+			in = false
+		}
+
+		if in {
+			c = mask
+		}
+
+		buf = append(buf, c)
+
+	}
+	fmt.Println(string(buf))
+
+}
