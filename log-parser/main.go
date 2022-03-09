9ffaf9680e94b103c4b4fe4ea76f269aa41371fc
Emirhan Öztürk <68188573+emirhanozturk@users.noreply.github.com>
2022-03-09 16:36:33 +0300

    log-parser rewrite to structs

--- log-parser/main.go
+++ log-parser/main.go
@@ -9,49 +9,66 @@ import (
 	"strings"
 )
 
+type result struct {
+	domain string
+	visits int
+}
+
+type parser struct {
+	sum     map[string]result //to count visits
+	domains []string          //for unique domains
+	total   int               // total visit
+	lines   int               // every domain's total visit
+}
+
 func main() {
 
-	var (
-		sum     map[string]int //to count visits
-		domains []string       //for unique domains
-		total   int            // total visit
-		lines   int            // every domain's total visit
-	)
-	sum = make(map[string]int)
+	p := parser{
+		sum: make(map[string]result), //initialize the map
+	}
 
 	in := bufio.NewScanner(os.Stdin)
 	for in.Scan() {
+		p.lines++
 
 		fields := strings.Fields(in.Text())
+		if len(fields) != 2 {
+			fmt.Printf("Wrong input: %v (line #%d)\n", fields, p.lines)
+			return
+		}
 
 		domain := fields[0]
 
 		visits, err := strconv.Atoi(fields[1])
 
 		if visits < 0 || err != nil {
-			fmt.Printf("wrong input: %q (line #%d)\n", fields[1], lines)
+			fmt.Printf("wrong input: %q (line #%d)\n", fields[1], p.lines)
 			return
 		}
 
-		if _, ok := sum[domain]; !ok {
-			domains = append(domains, domain)
+		if _, ok := p.sum[domain]; !ok {
+			p.domains = append(p.domains, domain)
 		}
 
-		total += visits
-		sum[domain] += visits
+		p.total += visits
+
+		p.sum[domain] = result{
+			domain: domain,
+			visits: visits + p.sum[domain].visits,
+		}
 
 	}
 
-	sort.Strings(domains)
+	sort.Strings(p.domains)
 
 	fmt.Printf("%-30s %10s\n", "DOMAIN", "VISITS")
 	fmt.Println(strings.Repeat("-", 45))
 
-	for _, domain := range domains {
-		visits := sum[domain]
-		fmt.Printf("%-30s %10d\n", domain, visits)
+	for _, domain := range p.domains {
+		parsed := p.sum[domain]
+		fmt.Printf("%-30s %10d\n", domain, parsed.visits)
 	}
-	fmt.Printf("\n%-30s %10d\n", "TOTAL", total)
+	fmt.Printf("\n%-30s %10d\n", "TOTAL", p.total)
 
 	if err := in.Err(); err != nil {
 		fmt.Println("> Err:", err)
